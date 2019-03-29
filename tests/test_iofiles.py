e30a8319bc977ed1f84597100e7744c1c84cb3ae
Aldwin Syahperdana <syahperdana@gmail.com>
2019-03-29 19:03:30 +0700

    Update test_iofiles.py
    Little fix

--- tests/test_iofiles.py
+++ tests/test_iofiles.py
@@ -3,10 +3,10 @@ import pytest
 from PasutBIG.iofile import backup, get_size, save
 
 def test_backup():
-	assert(backup('./tests/test.tar.gz', ['./tests/test1.csv', './tests/test2.csv'], 1000) == 'test.tar.gz')
+	assert(backup('./PasutBIG/test.tar.gz', ['./PasutBIG/iofile.py', './tests/libcsv.py'], 1000) == 'test.tar.gz')
 
 def test_get_size():
 	assert(get_size('./tests/test1.csv') == 1000)
 
 def test_save():
-	assert(save('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', './tests/jquery.min.js') == 'jquery.min.js')
+	save('http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', './tests/jquery.min.js').read()
