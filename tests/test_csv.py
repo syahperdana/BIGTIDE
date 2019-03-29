3d3db8664b8b3c63b8399f780ca0f0317a07655e
Aldwin Syahperdana <syahperdana@gmail.com>
2019-03-29 10:52:34 +0700

    Update test_csv.py

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,4 +1,11 @@
 import pytest
+import os
+
+cwd = os.getcwd()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+print(cwd)
+print(dir_path)
 
 from PasutBIG.ccsv import getlastrow, getsecondrow, savedb, savedbf
 
