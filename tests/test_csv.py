8dbd06b592319686ee65a60180fef12b4e349e6c
Aldwin Syahperdana <syahperdana@gmail.com>
2019-03-29 09:24:49 +0700

    Update test_csv.py
    workaround

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,4 +1,6 @@
 import pytest
+import sys
+import os
 
 sys.path.insert(0, os.path.abspath('.'))
 from PasutBIG.csv import getsecondrow, getlastrow, savedb, savedbf
@@ -10,7 +12,7 @@ def test_last_row():
 	assert(getlastrow('test2.csv') == ['2019/03/28 14:00:00','0000.69','0000.64'])
 
 def test_save_db():
-  assert(savedb('test1.csv') == "20190311Z132300-20190328Z140000.csv")
+	assert(savedb('test1.csv') == "20190311Z132300-20190328Z140000.csv")
 
 def test_save_dbf():
-  assert(savedbf('test2.csv') == "20190311Z132300-20190328Z140000.csv")
+	assert(savedbf('test2.csv') == "20190311Z132300-20190328Z140000.csv")
