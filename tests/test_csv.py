e026c59afc036d5acc6cd873b4db9c6900f3a21b
Aldwin Syahperdana <syahperdana@gmail.com>
2019-03-29 08:03:37 +0700

    Create test_csv.py
    Testcase for csv function

---
+++ tests/test_csv.py
@@ -0,0 +1,15 @@
+import pytest
+
+from PasutBIG.csv import getsecondrow, getlastrow, savedb, savedbf
+
+def test_second_row():
+	assert(getsecondrow('test2.csv') == [2019/03/11 13:23:00,0000.70,0000.68])
+
+def test_last_row():
+	assert(getlastrow('test2.csv') == [2019/03/28 14:00:00,0000.69,0000.64])
+
+def test_save_db():
+  assert(savedb('test1.csv') == "20190311Z132300-20190328Z140000.csv")
+
+def test_save_dbf():
+  assert(savedbf('test2.csv') == "20190311Z132300-20190328Z140000.csv")
