31948d44093a853b160a7c0398b85d7d744a058d
Aldwin Syahperdana <syahperdana@gmail.com>
2019-04-24 20:19:22 +0700

    Small fix of file size information
    Print the file size in kilobytes instead of megabytes (because it's smaller than 1 MB).

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ def reporthook(count, block_size, total_size):
 	progress_size = int(count * block_size)
 	speed = int(progress_size / (1024 * duration))
 	percent = min(int(count*block_size*100/total_size),100)
-	sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-					(percent, progress_size / (1024 * 1024), speed, duration))
+	sys.stdout.write("\r...%d%%, %d KB, %d KB/s, %d seconds passed" %
+					(percent, progress_size / 1024, speed, duration))
 	sys.stdout.flush()
 
 def getsecondrow(filename):
